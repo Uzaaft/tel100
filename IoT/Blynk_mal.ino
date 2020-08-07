2cf5984124c8238d5a15dea00d22f83c325aa540
Uzair Aftab <muaf@nmbu.no>
2020-08-07 18:35:37 +0200

    Created Blynk example

---
+++ IoT/Blynk_mal.ino
@@ -0,0 +1,18 @@
+#include <blynk.h>
+
+
+
+const char auth[]= "Her Legger du inn Tokenen din";
+
+void setup(){
+	Blynk.begin(auth); //Her starter vi et prosess med Blynk.
+
+}
+
+void loop(){
+	Blynk.run();
+	/*
+	
+	*/
+	
+}
