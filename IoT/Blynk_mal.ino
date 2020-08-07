95f2cddb234b0f3b351afe52516ff44a083b4b43
Uzair Aftab <muaf@nmbu.no>
2020-08-07 20:34:17 +0200

    Finished Blynk example

--- IoT/Blynk_mal.ino
+++ IoT/Blynk_mal.ino
@@ -12,7 +12,12 @@ void setup(){
 void loop(){
 	Blynk.run();
 	/*
-	
+	Her kan du skrive koden du vil kjøre.
+	En viktig ting med Blynk er at Blynk har pins som er veldig like de du henter informasjon fra på particle eller arduinoen.  
+	Den eneste forskjellen er at disse er virtuelle, altså i skyen.
+	La oss si at du lagrer en variabel t med temperaturen du henter i koden din, og du har satt opp Blynk til å hente et tall 
+	fra Virtual pin nr 1. Dermed vil koden din bli:
+	Blynk.virtualWrite(V1, t); 
 	*/
-	
+	Blynk.virtualWrite(V1, t);	
 }
