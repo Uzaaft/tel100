7e17968eb69ec0b342f967263f1feaba2318b187
navjordj <navjordj@gmail.com>
2020-08-10 13:40:27 +0200

    lagt til hello world i CPP

---
+++ arduino/Forelesning 3/helloworld.cpp
@@ -0,0 +1,13 @@
+// Example program
+#include <iostream>
+
+int main()
+{
+    // Dette er en kommentar
+    
+    std::cout << "Hei fra TEL100"; // Husk semikolonet
+    
+    /*
+    Kommentar over flere linjer
+    */
+}
